b7bec6b298e17ea9ea67268e9bef797ce1931632
runinbk <runinbk@gmail.com>
2022-11-02 21:18:37 -0400

    Opciones de git

--- controller/CU4.GestionarRoles.js
+++ controller/CU4.GestionarRoles.js
@@ -11,12 +11,10 @@ export const getRoles = async (req, res) => {
 export const getRol = async (req, res) => {
   try {
     const [result] = await pool.query("select * from ROL where id=?", [
-      req.params.id
+      req.params.id,
     ]);
     if (result.length === 0) {
-      return res
-        .status(400)
-        .json({ message: "No hay ningun rol resgistrado" });
+      return res.status(400).json({ message: "No hay ningun rol resgistrado" });
     }
     res.json(result[0]);
   } catch (error) {
@@ -25,18 +23,16 @@ export const getRol = async (req, res) => {
 };
 export const createRol = async (req, res) => {
   try {
-    const { nombre ,descripcion  } =
-    req.body;
-  const [result] = await pool.query(
-    "insert into ROL(nombre ,descripcion ) values(?,?)",
-    [ nombre ,descripcion]
-  );
-  res.json({
-    id: result.insertId,
-    nombre,
-    descripcion
-   
-  });
+    const { nombre, descripcion } = req.body;
+    const [result] = await pool.query(
+      "insert into ROL(nombre ,descripcion ) values(?,?)",
+      [nombre, descripcion]
+    );
+    res.json({
+      id: result.insertId,
+      nombre,
+      descripcion,
+    });
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
@@ -45,7 +41,7 @@ export const updateRol = async (req, res) => {
   try {
     const [result] = await pool.query("update ROL set ? where id=?", [
       req.body,
-      req.params.id
+      req.params.id,
     ]);
     res.json(result);
   } catch (error) {
@@ -57,12 +53,16 @@ export const deleteRol = async (req, res) => {
     const [result] = await pool.query("delete from ROL where id=?", [
       req.params.id,
     ]);
-    if(result.affectedRows === 0)
-    {
-      return res.status(404).json({message:'No se encuentra ningun rol'})
+    if (result.affectedRows === 0) {
+      return res.status(404).json({ message: "No se encuentra ningun rol" });
     }
     return res.sendStatus(204);
   } catch (error) {
     return res.status(500).json({ message: error.message });
-  }1
+  }
+  1;
 };
+
+// Path: ProyectoSi1\controller\CU4.GestionarRoles.js
+// Compare this snippet from ProyectoSi1\config.js:
+// export const PORT=4000
