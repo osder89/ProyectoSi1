5f0ffb1b389f3dd52d26f833e09d0f90e82da092
Oscar Del Rio <oscardelrio89@gmail.com>
2022-11-03 16:51:06 -0400

    Prueba del caso 1

--- controller/CU1.IniciarSesion.js
+++ controller/CU1.IniciarSesion.js
@@ -1,2 +1,51 @@
 import { pool } from "../db/db.js";
 
+// http://localhost:3000/
+app.get('/', function(request, response) {
+	// Render login template
+	response.sendFile(path.join(__dirname + '/login.html'));
+});
+
+// http://localhost:3000/auth
+app.post('/auth', function(request, response) {
+	// Capture the input fields
+	let username = request.body.username;
+	let password = request.body.password;
+	// Ensure the input fields exists and are not empty
+	if (username && password) {
+		// Execute SQL query that'll select the account from the database based on the specified username and password
+		 pool.query('SELECT * FROM USUARIO WHERE login = ? AND Password = ?', [username, password], function(error, results, fields) {
+			// If there is an issue with the query, output the error
+			if (error) throw error;
+			// If the account exists
+			if (results.length > 0) {
+				// Authenticate the user
+				request.session.loggedin = true;
+				request.session.username = username;
+				// Redirect to home page
+				response.redirect('/home');
+			} else {
+				response.send('Incorrect Username and/or Password!');
+			}			
+			response.end();
+		});
+	} else {
+		response.send('Please enter Username and Password!');
+		response.end();
+	}
+});
+
+// http://localhost:3000/home
+app.get('/home', function(request, response) {
+	// If the user is loggedin
+	if (request.session.loggedin) {
+		// Output username
+		response.send('Welcome back, ' + request.session.username + '!');
+	} else {
+		// Not logged in
+		response.send('Please login to view this page!');
+	}
+	response.end();
+});
+
+app.listen(3000);
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import express from "express";
 import cors from "cors";
+import session  from "express-session";
+import path from "path";
 
 import { PORT } from "./config.js";
 import Routes from "./routes/Casos.routes.js";
@@ -10,7 +12,14 @@ app.use(
     origin: "*",
   })
 );
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
 app.use(Routes);
 app.listen(PORT);
+/*app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'static')));*/
 
 console.log(`Server is running on port ${PORT}`);
