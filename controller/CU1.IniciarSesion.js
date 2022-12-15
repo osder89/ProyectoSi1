a7abdc9c9338e12aa9b651ef934b0e28f212182a
Sergio Mauricio Nuñez <106033664+iamsergio7@users.noreply.github.com>
2022-12-15 01:22:34 -0400

    test login y logout

--- controller/CU1.IniciarSesion.js
+++ controller/CU1.IniciarSesion.js
@@ -1,115 +1,115 @@
-// import { pool } from "../db/db.js";
-// import bcrypt from 'bcrypt'
+import { pool } from "../db/db.js";
+import bcrypt from 'bcrypt'
 
 
-// export const login = async (req, res) => {
-// 	if (req.session.loggedin != true) {
-// 		// res.render('login/index');         //aqui vala ruta del html, la vista
-// 		res.json({ message: "Redireccionando a: /login" });
-// 	} else {
-// 		// res.redirect('/');
-// 		res.json({ message: "Redireccionando a: /" });
-// 	}
-// };
+export const login = async (req, res) => {
+	if (req.session.loggedin != true) {
+		// res.render('login/index');         //aqui vala ruta del html, la vista
+		res.json({ message: "Redireccionando a: /login" });
+	} else {
+		// res.redirect('/');
+		res.json({ message: "Redireccionando a: /" });
+	}
+};
 
-// export const auth = async (req, res) => {  //verifica la contrase;a e inicia sesion
-// 	try {
-// 		const data = req.body;
-// 		const [result] = await pool.query('SELECT * FROM USUARIO WHERE  login = ?', [data.login]);
+export const auth = async (req, res) => {  //verifica la contrase;a e inicia sesion
+	try {
+		const data = req.body;
+		const [result] = await pool.query('SELECT * FROM USUARIO WHERE  login = ?', [data.login]);
 
 
-// 		if (result.length > 0) { //el usuario si existe
-// 			result.forEach(async element => {
+		if (result.length > 0) { //el usuario si existe
+			result.forEach(async element => {
 				
-// 				// const password = data.Password;
-// 				await bcrypt.compare(data.Password, element.Password, (err, isMatch) => {
-// 					if (!isMatch) {
-// 						// res.render('login/index', {error: 'Error: contase;a incorrecta'});
-// 						res.json({ message: "Contraseña Incorrecta, redireccionando a: /login" });
-// 					} else {
-// 						req.session.loggedin = true;   //sesion logueada
-// 						req.session.name = element.name;
-// 						// res.redirect('/');      // ("/")  <-- es la ruta raiz o el home
-// 						res.json({ message: "Login correcto, redireccionando a: /" });
-// 					}
-// 				});
-// 			});
-// 		} else { //el usuario no existe
-// 			// res.render('login/index', { error: 'Error: el usuario no existe' })
-// 			res.json({ message: "Usuario no existe, redireccionando a: /login/index" });
-// 		}
-// 		res.json({
-// 			login
-// 		});
-// 	} catch (error) {
-// 		return res.status(500).json({ message: error.message });
-// 	}
-// }
+				// const password = data.Password;
+				await bcrypt.compare(data.Password, element.Password, (err, isMatch) => {
+					if (!isMatch) {
+						// res.render('login/index', {error: 'Error: contase;a incorrecta'});
+						res.json({ message: "Contraseña Incorrecta, redireccionando a: /login" });
+					} else {
+						req.session.loggedin = true;   //sesion logueada
+						req.session.name = element.name;
+						// res.redirect('/');      // ("/")  <-- es la ruta raiz o el home
+						res.json({ message: "Login correcto, redireccionando a: /" });
+					}
+				});
+			});
+		} else { //el usuario no existe
+			// res.render('login/index', { error: 'Error: el usuario no existe' })
+			res.json({ message: "Usuario no existe, redireccionando a: /login/index" });
+		}
+		res.json({
+			login
+		});
+	} catch (error) {
+		return res.status(500).json({ message: error.message });
+	}
+}
 
-// export const register = async (req, res) => {
-// 	if (res.session.loggedin != true) {
-// 		// res.render('login/register');         //aqui vala ruta del html, la vista
-// 		res.json({ message: "Redireccionando a: login/register" });
-// 	} else {
-// 		// res.redirect('/');
-// 		res.json({ message: "Redireccionando a: /" });
-// 	}
-// };
-
-
-// export const storeUser = async (req, res) => {
-
-// 	try {
-// 		const data = req.body;
-// 		const [result] = await pool.query('SELECT * FROM USUARIO WHERE login = ?', [data.login]);
-
-// 		if (result.length > 0) {
-// 			// return res.status(400).json({ message: "El usuario ya existe" });
-// 			// res.render('login/register', { error: 'Error: el usuario ya existe' });  //(login/register)-->ahi va la ubicacion de la pagina del formulario(vistas el html)
-// 			res.json({ message: "El usuario ya existe, redireccionando a: /login/register" });
-// 		} else {
-// 			const password = data.Password;
-// 			const encryptedPassword = await bcrypt.hash(password, 12)
-// 			let users = {
-// 				"login": req.body.login,
-// 				"Password": encryptedPassword,
-// 				"idEmpleado": req.body.idEmpleado,
-// 				"idRol": req.body.idRol
-// 			}
+export const register = async (req, res) => {
+	if (res.session.loggedin != true) {
+		// res.render('login/register');         //aqui vala ruta del html, la vista
+		res.json({ message: "Redireccionando a: login/register" });
+	} else {
+		// res.redirect('/');
+		res.json({ message: "Redireccionando a: /" });
+	}
+};
 
 
-// 			const [result] = await pool.query("insert into USUARIO(login, Password, idEmpleado, idRol ) values(?,?,?,?)",[ users.login, users.Password, users.idEmpleado, users.idRol],
+export const storeUser = async (req, res) => {
+
+	try {
+		const data = req.body;
+		const [result] = await pool.query('SELECT * FROM USUARIO WHERE login = ?', [data.login]);
+
+		if (result.length > 0) {
+			// return res.status(400).json({ message: "El usuario ya existe" });
+			// res.render('login/register', { error: 'Error: el usuario ya existe' });  //(login/register)-->ahi va la ubicacion de la pagina del formulario(vistas el html)
+			res.json({ message: "El usuario ya existe, redireccionando a: /login/register" });
+		} else {
+			const password = data.Password;
+			const encryptedPassword = await bcrypt.hash(password, 12)
+			let users = {
+				"login": req.body.login,
+				"Password": encryptedPassword,
+				"idEmpleado": req.body.idEmpleado,
+				"idRol": req.body.idRol
+			}
+
+
+			const [result] = await pool.query("insert into USUARIO(login, Password, idEmpleado, idRol ) values(?,?,?,?)",[ users.login, users.Password, users.idEmpleado, users.idRol],
 				
-// 				function (error, results, fields) {
+				function (error, results, fields) {
 					
-// 					if (error) {
-// 						res.send({
-// 							"code": 400,
-// 							"failed": "error occurred",
-// 							"error": error
-// 						})
-// 					} else {
-// 						res.send({
-// 							"code": 200,
-// 							"success": "USUARIO REGISTRADO SATISFACTORIAMENTE"
+					if (error) {
+						res.send({
+							"code": 400,
+							"failed": "error occurred",
+							"error": error
+						})
+					} else {
+						res.send({
+							"code": 200,
+							"success": "USUARIO REGISTRADO SATISFACTORIAMENTE"
 	
-// 						});
-// 					}
-// 				}
-// 			);
-// 			res.json({
-// 				id: result.insertId,
-// 				login: users.login, 
-// 				Password:users.Password, 
-// 				idEmpleado: users.idEmpleado, 
-// 				idRol:users.idRol
-// 			});
-// 			req.session.loggedin = true;   //sesion logueada
-// 			req.session.name = data.name;
-// 			res.json({ message: "Sesion iniciada con exito" });
-// 		}
+						});
+					}
+				}
+			);
+			res.json({
+				id: result.insertId,
+				login: users.login, 
+				Password:users.Password, 
+				idEmpleado: users.idEmpleado, 
+				idRol:users.idRol
+			});
+			req.session.loggedin = true;   //sesion logueada
+			req.session.name = data.name;
+			res.json({ message: "Sesion iniciada con exito" });
+		}
 		
-// 	} catch (error) {
-// 		console.log(error);
-// 	}
-// };
+	} catch (error) {
+		console.log(error);
+	}
+};
