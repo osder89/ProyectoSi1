7bd148ffc1148eebc235a6d94a294cb445cd4662
Sergio Mauricio Nuñez <106033664+iamsergio7@users.noreply.github.com>
2022-12-15 01:46:30 -0400

    test 6 login y logout

--- controller/CU2.CerrarSesion.js
+++ controller/CU2.CerrarSesion.js
@@ -3,8 +3,7 @@ export const logout = async (req, res) => {
     try {
         
         req.session.loggedin = false;
-        res.json({ message: "Cerro Sesion" });
-        res.json({ message: "Redireccionando a: /" });
+        res.json({ message: "Cerro Sesion, Redireccionando a: /" });
         // res.redirect('/');
     } catch (error) {
         return res.status(500).json({ message: error.message });
